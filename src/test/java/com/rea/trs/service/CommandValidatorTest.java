2facea1b16656f9d89119fed244888dc40d86ee8
Naresh Miriyala <Naresh.Miriyala@jemena.com.au>
2016-07-10 00:49:33 +1000

    Fixing test

--- src/test/java/com/rea/trs/service/CommandValidatorTest.java
+++ src/test/java/com/rea/trs/service/CommandValidatorTest.java
@@ -4,6 +4,7 @@ import com.rea.trs.exceptions.InvalidCommandException;
 import com.rea.trs.model.InputStore;
 import com.rea.trs.test.AbstractTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -18,6 +19,12 @@ public class CommandValidatorTest extends AbstractTest {
     @BeforeMethod
     public void setUp() throws Exception {
         store.getCommands().add("PLACE 0,0,NORTH");
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        store.getCommands().clear();
 
     }
 
